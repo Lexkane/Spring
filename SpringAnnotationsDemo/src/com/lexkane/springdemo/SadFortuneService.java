06313d7b2a247d9ce37d8d2a0a7b55e9fb23cbe0
Lexkane <40840623+Lexkane@users.noreply.github.com>
2019-05-01 19:36:54 +0300

    Add files via upload

---
+++ SpringAnnotationsDemo/src/com/lexkane/springdemo/SadFortuneService.java
@@ -0,0 +1,9 @@
+package com.lexkane.springdemo;
+
+public class SadFortuneService implements FortuneService {
+
+  @Override
+  public String getFortune() {
+    return "Today is a sad day";
+  }
+}
